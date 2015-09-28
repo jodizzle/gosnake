dab9da2084b48a796f7ece0daa965e8b71bf73f7
Jody Leonard <me@jodyleonard.com>
2015-09-27 21:27:53 -0400

    Add basic snake mechanics
    The Player type now contains a field for a slice of rectangles as
    opposed to just a singular rectangle. Rectangles are appended to the
    slice by player.Eat(), and player.SnakeMovement() controls the movement
    of all the rectangles in indices greater than zero.

--- game.go
+++ game.go
@@ -12,7 +12,7 @@ var snakeTime float64
 var spawnTime float64
 
 type Player struct {
-	r 			*tl.Rectangle
+	snake 		[]*tl.Rectangle
 	direction	string
 	prevX 		int
 	prevY 		int
@@ -46,13 +46,19 @@ func (player *Player) Update(screen *tl.Screen) {
 		switch player.direction {
 		case "right":
 			player.SetPosition(player.prevX+1, player.prevY)
+			//player.SnakeMovement(player.prevX+1, player.prevY)
 		case "left":
 			player.SetPosition(player.prevX-1, player.prevY)
+			//player.SnakeMovement(player.prevX-1, player.prevY)
 		case "up":
 			player.SetPosition(player.prevX, player.prevY-1)
+			//player.SnakeMovement(player.prevX, player.prevY-1)
 		case "down":
 			player.SetPosition(player.prevX, player.prevY+1)
+			//player.SnakeMovement(player.prevX, player.prevY+1)
 		}
+
+		player.SnakeMovement()
 	}
 
 	spawnTime += screen.TimeDelta()
@@ -68,6 +74,50 @@ func (player *Player) Update(screen *tl.Screen) {
 	}
 }
 
+// Old approach
+// func (player *Player) SnakeMovement(x, y int) {
+// 	// Appends a copy of the snake tail
+// 	tailX, tailY := player.snake[len(player.snake)-1].Position()
+// 	rect := tl.NewRectangle(tailX, tailY, 1, 1, tl.ColorGreen)
+// 	game.Screen().Level().AddEntity(rect)
+// 	player.snake = append(player.snake, rect)
+// 	// Set new position of head
+// 	player.SetPosition(x, y)
+// 	// Deletes old tail
+// 	// oldTail := player.snake[len(player.snake)-2]
+// 	// game.Screen().Level().RemoveEntity(oldTail)
+// 	// player.snake = append(player.snake[:len(player.snake)-2], player.snake[len(player.snake)-1:]...)
+// 	// Updates positions
+
+	// 	for i := 0; i < len(player.snake)-1; i++ {
+// 		prevX, prevY := player.snake[i].Position()
+// 		player.snake[i+1].SetPosition(prevX, prevY)
+// 	}
+// }
+
+
+func (player *Player) SnakeMovement() {
+	//Don't do anything if it's currently just the head
+	if len(player.snake) > 1 {
+
+		refSnake := make([]*tl.Rectangle, len(player.snake))
+		//Doesn't work because it 'copy' copies the pointers
+		// copy(refSnake, player.snake)
+		//Need Deep Copy:
+		for i := 0; i < len(player.snake); i++ {
+			prevX, prevY := player.snake[i].Position()
+			refSnake[i] = tl.NewRectangle(prevX, prevY, 1, 1, tl.ColorGreen)
+		}
+
+		for i := 1; i < len(player.snake)-1; i++ {
+			prevX, prevY := refSnake[i].Position()
+			player.snake[i+1].SetPosition(prevX, prevY)
+		}
+
+		player.snake[1].SetPosition(player.prevX, player.prevY)
+	}
+}
+
 func (player *Player) Draw(screen *tl.Screen) {
 	// screenWidth, screenHeight := screen.Size()
 	// x, y := player.entity.Position()
@@ -75,17 +125,37 @@ func (player *Player) Draw(screen *tl.Screen) {
 	
 	player.Update(screen)
 
-	player.r.Draw(screen)
+	for _,s := range player.snake {
+		s.Draw(screen)
+	}
 }
 
-// Remove rectangle add to snake length
+// Remove rectangle and add to snake length
 func (player *Player) Eat(rect *tl.Rectangle) {
 
 	// Have to delete from level entities, not screen
 	game.Screen().Level().RemoveEntity(rect)
 	game.Log("Entity Removed")
 
-
+	px, py := player.snake[len(player.snake)-1].Position()
+	switch player.direction {
+	case "right":
+		rect.SetPosition(px-1, py)
+		player.snake = append(player.snake, rect)
+		game.Screen().Level().AddEntity(rect)
+	case "left":
+		rect.SetPosition(px+1, py)
+		player.snake = append(player.snake, rect)
+		game.Screen().Level().AddEntity(rect)
+	case "up":
+		rect.SetPosition(px, py+1)
+		player.snake = append(player.snake, rect)
+		game.Screen().Level().AddEntity(rect)
+	case "down":
+		rect.SetPosition(px, py-1)
+		player.snake = append(player.snake, rect)
+		game.Screen().Level().AddEntity(rect)
+	}
 }
 
 //Order seems to be Tick then Draw, but only if there is an event to activate Tick
@@ -120,15 +190,15 @@ func (player *Player) Tick(event tl.Event) {
 }
 
 func (player *Player) Size() (int, int) {
-	return player.r.Size()
+	return player.snake[0].Size()
 }
 
 func (player *Player) Position() (int, int) {
-	return player.r.Position()
+	return player.snake[0].Position()
 }
 
 func (player *Player) SetPosition(x, y int) {
-	player.r.SetPosition(x, y)
+	player.snake[0].SetPosition(x, y)
 }
 
 func (player *Player) Collide(collision tl.Physical) {
@@ -151,7 +221,7 @@ func main() {
 	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
 
 	player := Player{
-		r:		tl.NewRectangle(50, 50, 1, 1, tl.ColorRed),
+		snake:	[]*tl.Rectangle{tl.NewRectangle(50, 50, 1, 1, tl.ColorRed)},
 		level:	level,
 	}
 
